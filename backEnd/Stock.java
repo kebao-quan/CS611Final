d43f9995a42e5f82d932ec589fb8535170e2ed80
Kebao Quan <kbquan@bu.edu>
2024-05-08 11:22:56 +0800

    update

--- backEnd/Stock.java
+++ backEnd/Stock.java
@@ -13,6 +13,8 @@ public class Stock implements Serializable {
     String name = "";
     double price = 0.0;
 
+    double boughtPrice = 0.0;
+
     public Stock(String symbol, String name, double price) {
         this.symbol = symbol;
         this.name = name;
@@ -29,6 +31,18 @@ public class Stock implements Serializable {
 
     public double getPrice() {
         return price;
+    }
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public void setBoughtPrice(double boughtPrice) {
+        this.boughtPrice = boughtPrice;
+    }
+
+    public double calculateProfit() {
+        return price - boughtPrice;
     }
 
     @Override
